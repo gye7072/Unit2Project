f4cfa3424f8591b3e1d672debf3b6df2a15a65a5
Unit2Project <garyye143@gmail.com>
2022-10-12 12:28:52 -0400

    Finished

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args){
+        //
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Enter the first x-y coordinate point: ");
+        String s1 = scan1.nextLine();
+        int x1 = Integer.parseInt(s1.substring(1,s1.indexOf(",")));
+        int y1 = Integer.parseInt(s1.substring(s1.indexOf(",") + 1, s1.length() -1));
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the first x coordinate point: ");
-        int x1 = s.nextInt();
-        System.out.print("Enter the first y coordinate point: ");
-        int y1 = s.nextInt();
         System.out.print("Enter the second x coordinate point: ");
-        int x2 = s.nextInt();
-        System.out.print("Enter the second y coordinate point: ");
-        int y2 = s.nextInt();
+        Scanner scan2 = new Scanner(System.in);
+        String s2 = scan2.nextLine();
+        int x2 = Integer.parseInt(s2.substring(1,s2.indexOf(",")));
+        int y2 = Integer.parseInt(s2.substring(s2.indexOf(",") + 1,s2.length() -1));
+
 
         LinearEquation linear1 = new LinearEquation(x1, y1, x2, y2);
         System.out.print(linear1);
 
 
         System.out.print("Enter an x-value: ");
-        double x3 = s.nextDouble();
-        LinearEquation linear2 = new LinearEquation(x3);
-        System.out.print(linear2.calculateY());
-
-
-
+        Scanner scan3 = new Scanner(System.in);
+        double x3 = scan3.nextDouble();
+        System.out.println("Solved coordinate point is: (" + x3 + "," + linear1.calculateY(x3) + ")");
     }
 }
 
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,46 +1,53 @@
+import java.text.DecimalFormat;
 public class LinearEquation {
+    //allows rounding to the nearest hundredths possible
+    DecimalFormat df = new DecimalFormat("#.00");
     private int x1;
     private int y1;
     private int x2;
     private int y2;
-    private double x3;
+
+    double x3;
 
 
-    public LinearEquation(int x1, int y1, int x2, int y2){
+    public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
-    public LinearEquation(double x3){
-        this.x3 = x3;
+
+    public double slope() {
+        return (double) (y2 - y1) / (x2 - x1);
     }
 
-    public double slope(){
-        return (double) (y2-y1)/(x2-x1);
-    }
-    public int yIntercept(){
+    public int yIntercept() {
         return (int) (y1 - slope() * x1);
     }
-    public String linearEquation(){
-        return (y2-y1) + "/" + (x2-x1) + "x" + " + " + yIntercept();
+
+    public String linearEquation() {
+        return (y2 - y1) + "/" + (x2 - x1) + "x" + " + " + yIntercept();
     }
 
-    public double distance(){
-        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-    }
-    public String calculateY(){
-        double y3 = ((y2-y1)/(x2-x1)) * x3 + yIntercept();
-        return "Solved coordinate plane: (" + x3 + "," + y3 + ")";
+    public double distance() {
+        return (Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)));
     }
 
-    public String toString(){
-        String firstPair =  "First pair: (" + x1 + "," + y1 + ")" + "\n";
+    public double calculateY(double x3) {
+        this.x3 = x3;
+        return slope() * x3 + yIntercept();
+    }
+
+    public String toString() {
+        String firstPair = "First pair: (" + x1 + "," + y1 + ")" + "\n";
         String secondPair = "Second pair: (" + x2 + "," + y2 + ")" + "\n";
         String slopeOfLine = "Slope of Line: " + slope() + "\n";
         String yIntercept = "Y-Intercept: " + yIntercept() + "\n";
-        String linearEquation =  "Slope Intercept Form: " + linearEquation() + "\n";
-        String distance = "Distance between points: " + distance() + "\n";
+        String linearEquation = "Slope Intercept Form: " + linearEquation() + "\n";
+        String distance = "Distance between points: " + df.format(distance()) + "\n";
         return firstPair + secondPair + slopeOfLine + yIntercept + linearEquation + distance;
     }
+
 }
+
+
